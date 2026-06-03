993cc95caffe0b432808b02609cfdf51bf8187c1
salva <salvaalca52@gmail.com>
2026-06-03 00:33:55 -0500

    fix: replace FeatherIcon with lucide-vue-next icons in InstallPrompt to prevent crash on mobile

--- frontend/src/components/InstallPrompt.vue
+++ frontend/src/components/InstallPrompt.vue
@@ -14,7 +14,7 @@
 		</template>
 		<template #actions>
 			<Button variant="solid" class="w-full py-5" @click="install">
-				<template #prefix><FeatherIcon name="download" class="w-4" /></template>
+				<template #prefix><Download class="w-4" /></template>
 				{{ __('Install') }}
 			</Button>
 		</template>
@@ -32,9 +32,8 @@
 						{{ __('Install Frappe Learning') }}
 					</span>
 					<span class="inline-flex items-baseline">
-						<FeatherIcon
-							name="x"
-							class="ms-auto h-4 w-4 text-gray-700"
+						<X
+							class="ms-auto h-4 w-4 text-gray-700 cursor-pointer"
 							@click="iosInstallMessage = false"
 						/>
 					</span>
@@ -50,7 +49,7 @@
 						</span>
 						<span class="inline-flex items-start whitespace-nowrap">
 							<span>{{ __('Tap') }}&nbsp;</span>
-							<FeatherIcon name="share" class="h-4 w-4 text-blue-600" />
+							<Share class="h-4 w-4 text-blue-600" />
 							<span>&nbsp;{{ __("and then 'Add to Home Screen'") }}</span>
 						</span>
 					</span>
@@ -62,7 +61,8 @@
 
 <script setup>
 import { ref } from 'vue'
-import { Button, Dialog, FeatherIcon, Popover } from 'frappe-ui'
+import { Button, Dialog, Popover } from 'frappe-ui'
+import { Download, Share, X } from 'lucide-vue-next'
 
 const deferredPrompt = ref(null)
 const showDialog = ref(false)
