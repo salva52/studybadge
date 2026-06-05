6511eaa14e8c5d9415973eba639f1fc0410c4180
salva <salvaalca52@gmail.com>
2026-06-04 20:35:11 -0500

    fix: mobile logo display, referral cookie error, and signup form feedback

--- lms/lms/referral.py
+++ lms/lms/referral.py
@@ -5,7 +5,7 @@ from frappe.utils import get_url
 def track_referral_click(ref_code):
     if not ref_code:
         return
-    frappe.local.cookie_manager.set_cookie("studybadge_ref", ref_code, expires_in_days=30)
+    frappe.local.cookie_manager.set_cookie("studybadge_ref", ref_code, max_age=2592000)
     return {"status": "success"}
 
 def on_user_creation(doc, method):
